d165bcdfc966dd73b2440036517a0f4890e784cd
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-11-12 12:51:09 +0100

    Added UpdaterResult

--- MSBuildPropsUpdater/MSBuildPropsUpdater/Updater.cs
+++ MSBuildPropsUpdater/MSBuildPropsUpdater/Updater.cs
@@ -1,18 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 
 namespace MSBuildPropsUpdater
 {
-    public class Updater
+    public static class Updater
     {
-        public IList<XDocument> Documents { get; set; }
-        public IList<PackageReference> References { get; set; }
-        public Dictionary<string, List<PackageReference>> GroupedReferences { get; set; }
-
         public static string NormalizePath(string path)
         {
             return path.Replace(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar).ToUpperInvariant();
@@ -25,31 +20,36 @@ namespace MSBuildPropsUpdater
                 {
                     if (!ignoredPaths.Any(i => NormalizePath(fileName).Contains(NormalizePath(i))))
                     {
-                        var document = XDocument.Load(fileName);
-                        documents.Add(document);
-                        foreach (var reference in document.Descendants().Where(x => x.Name.LocalName == "PackageReference"))
-                        {
-                            var name = reference.Attribute("Include").Value;
-                            var versionAttribute = reference.Attribute("Version");
-                            var version = versionAttribute != null ? versionAttribute.Value : reference.Elements().First(x => x.Name.LocalName == "Version").Value;
-                            var pr = new PackageReference()
-                            {
-                                Name = name,
-                                Version = version,
-                                FileName = fileName,
-                                Document = document,
-                                Reference = reference,
-                                VersionAttribute = versionAttribute
-                            };
-                            references.Add(pr);
-                        }
+                        FindReferences(fileName, references, documents);
                     }
                 });
         }
 
-        public static Updater Create(string searchPath, string searchPattern, IEnumerable<string> ignoredPaths)
+        public static void FindReferences(string fileName, IList<PackageReference> references, IList<XDocument> documents)
         {
-            var updater = new Updater()
+            var document = XDocument.Load(fileName);
+            documents.Add(document);
+            foreach (var reference in document.Descendants().Where(x => x.Name.LocalName == "PackageReference"))
+            {
+                var name = reference.Attribute("Include").Value;
+                var versionAttribute = reference.Attribute("Version");
+                var version = versionAttribute != null ? versionAttribute.Value : reference.Elements().First(x => x.Name.LocalName == "Version").Value;
+                var pr = new PackageReference()
+                {
+                    Name = name,
+                    Version = version,
+                    FileName = fileName,
+                    Document = document,
+                    Reference = reference,
+                    VersionAttribute = versionAttribute
+                };
+                references.Add(pr);
+            }
+        }
+
+        public static UpdaterResult FindReferences(string searchPath, string searchPattern, IEnumerable<string> ignoredPaths)
+        {
+            var updater = new UpdaterResult()
             {
                 Documents = new List<XDocument>(),
                 References = new List<PackageReference>()
@@ -62,37 +62,37 @@ namespace MSBuildPropsUpdater
             return updater;
         }
 
-        public void PrintVersions()
+        public static void PrintVersions(this UpdaterResult result)
         {
-            Debug.WriteLine("NuGet package dependencies versions:");
-            foreach (var package in GroupedReferences)
+            Console.WriteLine("NuGet package dependencies versions:");
+            foreach (var package in result.GroupedReferences)
             {
-                Debug.WriteLine($"Package {package.Key} is installed:");
+                Console.WriteLine($"Package {package.Key} is installed:");
                 foreach (var v in package.Value)
                 {
-                    Debug.WriteLine($"{v.Version}, {v.FileName}");
+                    Console.WriteLine($"{v.Version}, {v.FileName}");
                 }
             }
         }
 
-        public void ValidateVersions()
+        public static void ValidateVersions(this UpdaterResult result)
         {
-            Debug.WriteLine("Checking installed NuGet package dependencies versions:");
-            foreach (var package in GroupedReferences)
+            Console.WriteLine("Checking installed NuGet package dependencies versions:");
+            foreach (var package in result.GroupedReferences)
             {
                 var packageVersion = package.Value.First().Version;
                 bool isValidVersion = package.Value.All(x => x.Version == packageVersion);
                 if (!isValidVersion)
                 {
-                    Debug.WriteLine($"Error: package {package.Key} has multiple versions installed:");
+                    Console.WriteLine($"Error: package {package.Key} has multiple versions installed:");
                     foreach (var v in package.Value)
                     {
-                        Debug.WriteLine($"{v.Version}, {v.FileName}");
+                        Console.WriteLine($"{v.Version}, {v.FileName}");
                     }
                     throw new Exception("Detected multiple NuGet package version installed for different projects.");
                 }
             };
-            Debug.WriteLine("All NuGet package dependencies versions are valid.");
+            Console.WriteLine("All NuGet package dependencies versions are valid.");
         }
     }
 }
---
+++ MSBuildPropsUpdater/MSBuildPropsUpdater/UpdaterResult.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace MSBuildPropsUpdater
+{
+    public class UpdaterResult
+    {
+        public IList<XDocument> Documents { get; set; }
+        public IList<PackageReference> References { get; set; }
+        public Dictionary<string, List<PackageReference>> GroupedReferences { get; set; }
+    }
+}
