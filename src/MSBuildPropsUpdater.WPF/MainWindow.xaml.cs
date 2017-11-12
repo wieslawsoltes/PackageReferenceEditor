a23d91c5e309ecbbd408d107b73250cf43747454
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-11-12 13:24:30 +0100

    Added update button

--- src/MSBuildPropsUpdater.WPF/MainWindow.xaml.cs
+++ src/MSBuildPropsUpdater.WPF/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
 
 namespace MSBuildPropsUpdater.WPF
 {
@@ -14,7 +18,47 @@ namespace MSBuildPropsUpdater.WPF
             //var result = Updater.FindReferences(@"C:\DOWNLOADS\GitHub\", "*.props", new string[] { });
             //result.PrintVersions();
             //result.ValidateVersions();
+
             DataContext = Updater.FindReferences(textSearchPath.Text, textSearchPattern.Text, new string[] { });
+        }
+
+        private void buttonUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            if (groups.SelectedItem is KeyValuePair<string, List<PackageReference>> references)
+            {
+                foreach (var v in references.Value)
+                {
+                    try
+                    {
+                        if (v.VersionAttribute != null)
+                        {
+                            if (v.Version != v.VersionAttribute.Value)
+                            {
+                                Debug.WriteLine($"Old: {v.VersionAttribute.Value}, New: {v.Version}, File: {v.FileName}");
+                                v.VersionAttribute.Value = v.Version;
+                                v.Document.Save(v.FileName);
+                            }
+                        }
+                        else
+                        {
+                            var version = v.Reference.Elements().First(x => x.Name.LocalName == "Version");
+                            if (version != null)
+                            {
+                                if (v.Version != v.VersionAttribute.Value)
+                                {
+                                    Debug.WriteLine($"Old: {version.Value}, New: {v.Version}, File: {v.FileName}");
+                                    version.Value = v.Version;
+                                    v.Document.Save(v.FileName);
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
         }
     }
 }
