82b7d39ffa51953d8630501212106038acfb7194
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-11-14 12:25:04 +0100

    Create AssemblyInfo.cs

---
+++ src/PackageReferenceEditor.Avalonia/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PackageReferenceEditor.Avalonia")]
+[assembly: AssemblyDescription("MSBuild, csproj and props package reference editor.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Wiesław Šoltés")]
+[assembly: AssemblyProduct("PackageReferenceEditor.Avalonia")]
+[assembly: AssemblyCopyright("Copyright © Wiesław Šoltés 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
