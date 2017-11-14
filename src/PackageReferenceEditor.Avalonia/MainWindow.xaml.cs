aadb49fec67f1e9f2dfeb2a670d2dd63ffca59a6
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-11-14 12:10:05 +0100

    Create MainWindow.xaml.cs

---
+++ src/PackageReferenceEditor.Avalonia/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PackageReferenceEditor.Avalonia
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            this.AttachDevTools();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
