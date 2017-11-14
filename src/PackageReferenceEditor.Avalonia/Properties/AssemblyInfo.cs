e0b0ecd431559384c4711837ef9d4654ad364be7
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-11-14 17:44:25 +0100

    Updated version

--- src/PackageReferenceEditor.Avalonia/Properties/AssemblyInfo.cs
+++ src/PackageReferenceEditor.Avalonia/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@
 [assembly: AssemblyCopyright("Copyright © Wiesław Šoltés 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.0.2.0")]
--- src/PackageReferenceEditor.WPF/Properties/AssemblyInfo.cs
+++ src/PackageReferenceEditor.WPF/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.0.2.0")]
