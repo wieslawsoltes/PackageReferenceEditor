8515fd38d2890395a02a54930a390ffc7270f3e8
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-11-15 18:09:56 +0100

    Added NotifyObject class

---
+++ src/PackageReferenceEditor/NotifyObject.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PackageReferenceEditor
+{
+    public abstract class NotifyObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Notify([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public bool Update<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (!Equals(field, value))
+            {
+                field = value;
+                Notify(propertyName);
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- src/PackageReferenceEditor/PackageReference.cs
+++ src/PackageReferenceEditor/PackageReference.cs
@@ -2,13 +2,49 @@
 
 namespace PackageReferenceEditor
 {
-    public class PackageReference
+    public class PackageReference : NotifyObject
     {
-        public string Name { get; set; }
-        public string Version { get; set; }
-        public string FileName { get; set; }
-        public XDocument Document { get; set; }
-        public XElement Reference { get; set; }
-        public XAttribute VersionAttribute { get; set; }
+        private string _name;
+        private string _version;
+        private string _fileName;
+        private XDocument _document;
+        private XElement _reference;
+        private XAttribute _versionAttribute;
+
+        public string Name
+        {
+            get => _name;
+            set => Update(ref _name, value);
+        }
+
+        public string Version
+        {
+            get => _version;
+            set => Update(ref _version, value);
+        }
+
+        public string FileName
+        {
+            get => _fileName;
+            set => Update(ref _fileName, value);
+        }
+
+        public XDocument Document
+        {
+            get => _document;
+            set => Update(ref _document, value);
+        }
+
+        public XElement Reference
+        {
+            get => _reference;
+            set => Update(ref _reference, value);
+        }
+
+        public XAttribute VersionAttribute
+        {
+            get => _versionAttribute;
+            set => Update(ref _versionAttribute, value);
+        }
     }
 }
