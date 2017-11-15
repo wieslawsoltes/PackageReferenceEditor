40f3e9c3821f69cb088dcf8c1fa907682d3eec14
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-11-15 19:22:46 +0100

    Use LogToTrace

--- src/PackageReferenceEditor.Avalonia/App.xaml.cs
+++ src/PackageReferenceEditor.Avalonia/App.xaml.cs
@@ -10,45 +10,17 @@ namespace PackageReferenceEditor.Avalonia
 {
     public class App : Application
     {
-        static void Print(Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-            Console.WriteLine(ex.StackTrace);
-            if (ex.InnerException != null)
-            {
-                Print(ex.InnerException);
-            }
-        }
-
         static void Main(string[] args)
         {
-            try
-            {
-                InitializeLogging();
-
-                var app = new App();
-                AppBuilder.Configure(app)
-                    .UsePlatformDetect()
-                    .Start<MainWindow>(() => new UpdaterResult()
-                    {
-                        Documents = new List<XDocument>(),
-                        References = new List<PackageReference>()
-                    });
-            }
-            catch (Exception ex)
-            {
-                Print(ex);
-            }
-        }
-
-        static void InitializeLogging()
-        {
-#if DEBUG
-            SerilogLogger.Initialize(new LoggerConfiguration()
-                .MinimumLevel.Warning()
-                .WriteTo.Trace(outputTemplate: "{Area}: {Message}")
-                .CreateLogger());
-#endif
+            var app = new App();
+            AppBuilder.Configure(app)
+                .LogToTrace()
+                .UsePlatformDetect()
+                .Start<MainWindow>(() => new UpdaterResult()
+                {
+                    Documents = new List<XDocument>(),
+                    References = new List<PackageReference>()
+                });
         }
 
         public override void Initialize()
