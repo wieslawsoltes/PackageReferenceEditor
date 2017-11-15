8343b7f2629c21c6a970cb2f533a0016bce26bf9
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-11-15 19:03:58 +0100

    Updated bash script

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 dotnet restore src/PackageReferenceEditor
 dotnet build src/PackageReferenceEditor/PackageReferenceEditor.csproj -c Release -f netstandard2.0
-dotnet pack src/PackageReferenceEditor/PackageReferenceEditor.csproj -c Release
 
 dotnet restore src/PackageReferenceEditor.Avalonia
 dotnet build src/PackageReferenceEditor.Avalonia/PackageReferenceEditor.Avalonia.csproj -c Release -f netcoreapp2.0
-
-dotnet publish src/PackageReferenceEditor.Avalonia/PackageReferenceEditor.Avalonia.csproj -c Release -f netcoreapp2.0 -r win7-x64 -o bin/win7-x64
-dotnet publish src/PackageReferenceEditor.Avalonia/PackageReferenceEditor.Avalonia.csproj -c Release -f netcoreapp2.0 -r ubuntu.14.04-x64 -o bin/ubuntu.14.04-x64
-dotnet publish src/PackageReferenceEditor.Avalonia/PackageReferenceEditor.Avalonia.csproj -c Release -f netcoreapp2.0 -r osx.10.12-x64 -o bin/osx.10.12-x64
