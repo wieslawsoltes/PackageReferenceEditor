42f06abdb243e60f32e554b4449a952bc82816f1
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-11-16 08:37:06 +0100

    Update MainWindow.xaml.cs
    [skip ci]

--- src/PackageReferenceEditor.Avalonia/MainWindow.xaml.cs
+++ src/PackageReferenceEditor.Avalonia/MainWindow.xaml.cs
@@ -12,98 +12,11 @@ namespace PackageReferenceEditor.Avalonia
         {
             this.InitializeComponent();
             this.AttachDevTools();
-
-            var patterns = this.FindControl<DropDown>("patterns");
-            var textSearchPattern = this.FindControl<TextBox>("textSearchPattern");
-
-            patterns.SelectionChanged += (sender, e) =>
-            {
-                textSearchPattern.Text = (patterns.SelectedItem as DropDownItem).Content as string;
-            };
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
-        }
-
-        private async void buttonBrowse_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                var dlg = new OpenFolderDialog();
-                var path = await dlg.ShowAsync(this);
-                if (!string.IsNullOrWhiteSpace(path))
-                {
-                    var textSearchPath = this.FindControl<TextBox>("textSearchPath");
-                    textSearchPath.Text = path;
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                Console.WriteLine(ex.StackTrace);
-            }
-        }
-
-        private void buttonSearch_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                if (DataContext is UpdaterResult result)
-                {
-                    var textSearchPath = this.FindControl<TextBox>("textSearchPath");
-                    var textSearchPattern = this.FindControl<TextBox>("textSearchPattern");
-                    result.Reset();
-                    result.FindReferences(
-                        textSearchPath.Text,
-                        textSearchPattern.Text,
-                        new string[] { });
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                Console.WriteLine(ex.StackTrace);
-            }
-        }
-
-        private void buttonConsolidate_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                if (DataContext is UpdaterResult result)
-                {
-                    foreach (var reference in result.CurrentReferences.Value)
-                    {
-                        if (reference != result.CurrentReference)
-                        {
-                            reference.Version = result.CurrentReference.Version;
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                Console.WriteLine(ex.StackTrace);
-            }
-        }
-
-        private void buttonUpdate_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                if (DataContext is UpdaterResult result)
-                {
-                    Updater.UpdateVersions(result.CurrentReferences);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                Console.WriteLine(ex.StackTrace);
-            }
         }
     }
 }
