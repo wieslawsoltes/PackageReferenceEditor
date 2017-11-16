572bdd13c620d59c1c1a18e1a332bc087d035c1e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-11-16 17:59:54 +0100

    Updated version

--- src/PackageReferenceEditor.Avalonia/Properties/AssemblyInfo.cs
+++ src/PackageReferenceEditor.Avalonia/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@
 [assembly: AssemblyCopyright("Copyright © Wiesław Šoltés 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.0.3")]
-[assembly: AssemblyFileVersion("0.0.3")]
+[assembly: AssemblyVersion("0.0.4")]
+[assembly: AssemblyFileVersion("0.0.4")]
