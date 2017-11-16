d17c3f9b085ca6cc9eaf669d530e1615d22a5d38
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-11-16 18:23:53 +0100

    Added Feed class

---
+++ src/PackageReferenceEditor/Feed.cs
@@ -0,0 +1,27 @@
+﻿
+namespace PackageReferenceEditor
+{
+    public class Feed : NotifyObject
+    {
+        private string _name;
+        private string _uri;
+
+        public string Name
+        {
+            get => _name;
+            set => Update(ref _name, value);
+        }
+
+        public string Uri
+        {
+            get => _uri;
+            set => Update(ref _uri, value);
+        }
+
+        public Feed(string name, string uri)
+        {
+            Name = name;
+            Uri = uri;
+        }
+    }
+}
