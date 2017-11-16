3221d68e56c4551a08cc9fbde05973e928e4bab0
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-11-16 18:24:13 +0100

    Removed usings

--- src/PackageReferenceEditor.Avalonia/App.xaml.cs
+++ src/PackageReferenceEditor.Avalonia/App.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Xml.Linq;
 using Avalonia;
 using Avalonia.Logging.Serilog;
