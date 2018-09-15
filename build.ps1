5014b3c65273369d0414406f0ab414ace9b0412c
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2018-09-15 14:50:17 +0200

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -6,9 +6,9 @@ dotnet restore src/PackageReferenceEditor.Avalonia
 dotnet build src/PackageReferenceEditor.Avalonia/PackageReferenceEditor.Avalonia.csproj -c Release -f netcoreapp2.0
 
 dotnet publish src/PackageReferenceEditor.Avalonia/PackageReferenceEditor.Avalonia.csproj -c Release -f netcoreapp2.0 -r win7-x64 -o bin/win7-x64
-Copy-Item "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.12.25810\x64\Microsoft.VC141.CRT\msvcp140.dll" src/PackageReferenceEditor.Avalonia/bin/win7-x64
-Copy-Item "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.12.25810\x64\Microsoft.VC141.CRT\vcruntime140.dll" src/PackageReferenceEditor.Avalonia/bin/win7-x64
-& "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\114.12.25827\bin\HostX86\x86\editbin.exe" /subsystem:windows src/PackageReferenceEditor.Avalonia/bin/win7-x64/PackageReferenceEditor.Avalonia.exe
+Copy-Item "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.15.26706\x64\Microsoft.VC141.CRT\msvcp140.dll" src/PackageReferenceEditor.Avalonia/bin/win7-x64
+Copy-Item "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.15.26706\x64\Microsoft.VC141.CRT\vcruntime140.dll" src/PackageReferenceEditor.Avalonia/bin/win7-x64
+& "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.15.26726\bin\HostX86\x86\editbin.exe" /subsystem:windows src/PackageReferenceEditor.Avalonia/bin/win7-x64/PackageReferenceEditor.Avalonia.exe
 
 dotnet publish src/PackageReferenceEditor.Avalonia/PackageReferenceEditor.Avalonia.csproj -c Release -f netcoreapp2.0 -r ubuntu.14.04-x64 -o bin/ubuntu.14.04-x64
 dotnet publish src/PackageReferenceEditor.Avalonia/PackageReferenceEditor.Avalonia.csproj -c Release -f netcoreapp2.0 -r osx.10.12-x64 -o bin/osx.10.12-x64
