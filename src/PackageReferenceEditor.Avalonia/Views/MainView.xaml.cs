b1dc40217a1a5edc46a66faf156634c1cf3049d6
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-12-09 22:54:58 +0100

    Updated names

--- src/PackageReferenceEditor.Avalonia/Views/MainView.xaml.cs
+++ src/PackageReferenceEditor.Avalonia/Views/MainView.xaml.cs
@@ -7,10 +7,16 @@ namespace PackageReferenceEditor.Avalonia.Views
 {
     public class MainView : UserControl
     {
+        private readonly DropDown _dropDownPatterns;
+        private readonly Button _buttonBrowse;
+
         public MainView()
         {
             InitializeComponent();
-            this.FindControl<DropDown>("patterns").SelectionChanged += patterns_SelectionChanged;
+            _dropDownPatterns = this.FindControl<DropDown>("dropDownPatterns");
+            _dropDownPatterns.SelectionChanged += patterns_SelectionChanged;
+            _buttonBrowse= this.FindControl<Button>("buttonBrowse");
+            _buttonBrowse.Click += buttonBrowse_Click;
         }
 
         private void InitializeComponent()
@@ -20,9 +26,17 @@ namespace PackageReferenceEditor.Avalonia.Views
 
         private void patterns_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (DataContext is ReferenceEditor vm)
+            try
             {
-                vm.SearchPattern = this.FindControl<DropDown>("patterns").SelectedItem as string;
+                if (DataContext is ReferenceEditor vm)
+                {
+                    vm.SearchPattern = _dropDownPatterns.SelectedItem as string;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Log(ex.Message);
+                Logger.Log(ex.StackTrace);
             }
         }
 
